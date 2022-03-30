b706e06a1a2c24fad15fefeafa296b92c1ec282b
cbl <lennart.carbe@gmail.com>
2022-03-30 12:11:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Admin Laravel Package
+
+## Setup
+
+Install the package via composer
+
+```shell
+composer require macramejs/admin-laravel
+```
+
+Create an admin application:
+
+```shell
+php artisan make:admin admin
+```
+
+And follow the given instructions.
+
+## Media Manager
+
+```shell
+php artisan make:media-manager admin media
+```
+
+## Page Builder
+
+```shell
+php artisan make:page-builder admin page
+```
---
+++ src/Console/BaseMakeCommand.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace AwStudio\Wireframes\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+
+abstract class BaseMakeCommand extends Command
+{
+    /**
+     * The filesystem instance.
+     *
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    protected $files;
+
+    abstract protected function replaces(): array;
+
+    /**
+     * Create a new controller creator command instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem $files
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    protected function publishesPath($path = '')
+    {
+        return __DIR__."/../../publishes/{$this->publishes}/".($path != '' ? DIRECTORY_SEPARATOR.$path : '');
+    }
+
+    protected function publishFile($from, $to)
+    {
+        $this->files->copy($from, $to);
+
+        $this->applyReplacesToContent($to);
+    }
+
+    protected function publishDir($from, $to)
+    {
+        $this->files->ensureDirectoryExists($from);
+
+        $this->files->copyDirectory($from, $to);
+
+        $directoryIterator = new RecursiveDirectoryIterator($to);
+
+        collect(new RecursiveIteratorIterator($directoryIterator))
+            ->each(function (SplFileInfo $file) {
+                if (! $file->isDir()) {
+                    $this->applyReplacesToContent($file);
+                }
+
+                $this->applyReplacesToName($file);
+            });
+    }
+
+    protected function applyReplacesToName(string $file)
+    {
+        $baseName = basename($file);
+
+        foreach ($this->replaces() as $name => $replace) {
+            $baseName = str_replace('{{ '.$name.' }}', $replace, $baseName);
+        }
+
+        if ($baseName == basename($file)) {
+            return;
+        }
+
+        $newFile = Str::replaceLast(basename($file), $baseName, $file);
+
+        $this->files->move($file, $newFile);
+    }
+
+    protected function applyReplacesToContent(string $file)
+    {
+        $content = $this->files->get($file);
+
+        foreach ($this->replaces() as $name => $replace) {
+            $content = str_replace('{{ '.$name.' }}', $replace, $content);
+        }
+
+        $this->files->put($file, $content);
+    }
+
+    /**
+     * Insert code in the given file.
+     *
+     * @param  string $path
+     * @param  string $insert
+     * @param  string $after
+     * @return void
+     */
+    protected function insertAfter(string $path, string $insert, string $after)
+    {
+        $content = $this->files->get($path);
+
+        if (str_contains($content, $insert)) {
+            return;
+        }
+        $content = Str::replaceFirst($after, $after.PHP_EOL.$insert, $content);
+
+        $this->files->put($path, $content);
+
+        $this->info("{$path} changed, please check it for correction and formatting.");
+    }
+
+    public function insertBefore(string $path, string $insert, string $before)
+    {
+        $content = $this->files->get($path);
+
+        if (str_contains($content, $insert)) {
+            return;
+        }
+
+        $content = Str::replaceFirst($before, $insert.PHP_EOL.$before, $content);
+
+        $this->files->put($path, $content);
+
+        $this->info("{$path} changed, please check it for correction and formatting.");
+    }
+
+    /**
+     * Insert code at the end of the given file.
+     *
+     * @param  string $path
+     * @param  string $insert
+     * @return void
+     */
+    protected function insertAtEnd(string $path, string $insert)
+    {
+        $content = $this->files->get($path);
+
+        if (str_contains($content, $insert)) {
+            return;
+        }
+
+        $this->files->put($path, "{$content}\n\n{$insert}");
+
+        $this->info("{$path} changed, please check it for correction and formatting.");
+    }
+
+    /**
+     * Insert code at the end of the given file.
+     *
+     * @param  string $path
+     * @param  string $insert
+     * @return void
+     */
+    protected function insertAtStart(string $path, string $insert)
+    {
+        $content = $this->files->get($path);
+
+        if (str_contains($content, $insert)) {
+            return;
+        }
+
+        $this->files->put($path, "{$insert}\n{$content}");
+
+        $this->info("{$path} changed, please check it for correction and formatting.");
+    }
+}
