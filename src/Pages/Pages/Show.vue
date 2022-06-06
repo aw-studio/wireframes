e3545e8bf5ddb3b00fc1048eb844deda80b0582c
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-06-06 15:01:06 +0200

    fix header image

--- src/Pages/Pages/Show.vue
+++ src/Pages/Pages/Show.vue
@@ -1,20 +1,20 @@
 <template>
-	<Header />
-	<main class="mt-[90px] lg:mt-[189px]">
-		<Image
-			:src="page.attributes.header.url"
-			v-if="page.attributes.header.url"
-			class="w-full h-64"
-		/>
+    <Header />
+    <main class="mt-[90px] lg:mt-[189px]">
+        <Image
+            :src="page.attributes.header.url"
+            v-if="page.attributes.header?.url"
+            class="w-full h-64"
+        />
 
-		<div class="container">
-			<h1>
-				{{ page.attributes.h1 }}
-			</h1>
-		</div>
-		<Repeatables :repeatables="page.content" />
-	</main>
-	<Footer />
+        <div class="container">
+            <h1>
+                {{ page.attributes.h1 }}
+            </h1>
+        </div>
+        <Repeatables :repeatables="page.content" />
+    </main>
+    <Footer />
 </template>
 
 <script setup lang="ts">
@@ -25,17 +25,17 @@ import { Repeatables } from '../../modules/repeatables';
 import Image from '../../components/Ui/Image.vue';
 
 const props = defineProps({
-	page: {
-		required: true,
-		type: Object as PropType<Page>,
-	},
+    page: {
+        required: true,
+        type: Object as PropType<Page>,
+    },
 });
 </script>
 
 <style>
 #app {
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
 }
 </style>
